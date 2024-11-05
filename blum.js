4b709403d11e3c77c632a51c5b5a9891d1cc3419
cuadev <vutienanh291@gmail.com>
2024-11-06 02:06:41 +0700

    up

--- blum.js
+++ blum.js
@@ -38,19 +38,19 @@
     },
     {
       "id": "d2715289-b487-43bc-bc21-18224f8f6bc3",
-      "answer": "Nodoxxing"
+      "answer": "NODOXXING"
     },
     {
       "id": "7067a3db-d9c5-4268-ac19-c393743e8491",
       "answer": "WOWBLUM"
     },
     {
-      "id": "c60919cd-0282-46fe-854a-1da0a01db9b2",
-      "answer": "Blum - Big City Life"
-    },
-    {
       "id": "1572a605-d714-4f2c-8045-9c5f874d9c7e",
       "answer": "MEMEBLUM"
+    },
+    {
+      "id": "c60919cd-0282-46fe-854a-1da0a01db9b2",
+      "answer": "Blum - Big City Life"
     },
     {
       "id": "30d9f351-614e-4565-a1bb-e7e94fc3dc3c",
@@ -71,6 +71,38 @@
     {
       "id": "dc627a62-f747-4cbb-981f-62cf82a85458",
       "answer": "BLUMTASTIC"
+    },
+    {
+      "id": "71ad89ea-f11f-4825-af9c-408fba7dfd8e",
+      "answer": "Blumforce"
+    },
+    {
+      "id": "a669a160-45fd-4935-9eda-58079e19aad5",
+      "answer": "ULTRABLUM"
+    },
+    {
+      "id": "7491c933-e49d-4a60-89cd-53d9fe690dca",
+      "answer": "Blumstorm"
+    },
+    {
+      "id": "900bc6e5-d73e-49fe-adf5-1f8111f1b431",
+      "answer": "Blumextra"
+    },
+    {
+      "id": "6fb7499f-8b38-4132-8255-c3184cc2712c",
+      "answer": "PUMPIT"
+    },
+    {
+      "id": "98d390d1-95da-475f-8df9-53a335842c3a",
+      "answer": "Blumhelps"
+    },
+    {
+      "id": "92bc4338-85ca-4bf9-a0a5-320e677116fd",
+      "answer": "FOMOOO"
+    },
+    {
+      "id": "bb84e765-31aa-4f0d-8430-b3f75d88c1aa",
+      "answer": "CRYPTOZONE"
     },
   ]
 }
